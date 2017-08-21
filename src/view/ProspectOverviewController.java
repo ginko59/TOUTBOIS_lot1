a3076dd8a1b3525113fd26536ce3cb95ea5d4fef
aurelien <aur.les@hotmail.fr>
2017-08-21 11:12:22 +0200

    enchainement fenêtre menu fichier clientoverview,representantOverview, Prospectoverview

---
+++ src/view/ProspectOverviewController.java
@@ -0,0 +1,36 @@
+package view;
+
+import controller.MainApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+
+public class ProspectOverviewController {
+
+	@FXML
+    private Button bAjouter;
+    @FXML
+    private Button bModifier;
+    @FXML
+    private Button bSupprimer;
+    @FXML
+    private MenuItem mClient;
+    @FXML
+    private MenuItem mRepresentant;
+    @FXML
+    private MenuItem mProspect;
+    
+	@FXML
+	private void handleclient(){
+		MainApp.showClientOverview();
+	}
+	@FXML
+	private void handlerepresentant(){
+		MainApp.showrepresentantOverview();
+	}
+	@FXML
+	private void handleprospect(){
+		MainApp.showprospectOverview();
+	}
+
+}
