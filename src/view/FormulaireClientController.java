800d1e0838bf35c23e2c8948702145e8e6d26741
aurelien <aur.les@hotmail.fr>
2017-08-21 13:46:08 +0200

    création de tous les controller (certains bouton comme annuler, sauvegarder,supprimmer, le menu edit reste à implémenter)

---
+++ src/view/FormulaireClientController.java
@@ -0,0 +1,38 @@
+package view;
+
+import controller.MainApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+
+public class FormulaireClientController {
+
+	@FXML
+    private Button bSauvegarder;
+    @FXML
+    private Button bAnnuler;
+    @FXML
+    private MenuItem mClient;
+    @FXML
+    private MenuItem mRepresentant;
+    @FXML
+    private MenuItem mProspect;
+    
+	@FXML
+	private void handleclient(){
+		MainApp.showClientOverview();
+	}
+	@FXML
+	private void handlerepresentant(){
+		MainApp.showrepresentantOverview();
+	}
+	@FXML
+	private void handleprospect(){
+		MainApp.showprospectOverview();
+	}
+	@FXML
+	private void formulaireprospect(){
+		MainApp.showFormulaireProspect();
+	}
+
+}
